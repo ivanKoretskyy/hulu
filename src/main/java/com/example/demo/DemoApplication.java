1404cb6d656319b207d84676f2059d03dda7a47e
Ivan Koretskyy <ivan.koretskyy@MacBookPro.lan>
2024-11-08 14:14:22 -0600

    change to gradel

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.hulu;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HuluApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HuluApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
